10aa65e9312659257cf5410a6102780c683ca0f7
Veso-P <vesko_petkov@abv.bg>
2020-12-03 20:18:10 +0200

    Comments added to Blog-Item-Details component

--- src/app/blogs/blog.model.ts
+++ src/app/blogs/blog.model.ts
@@ -3,11 +3,13 @@ export class Blog {
   public name: string;
   public description: string;
   public imagePath: string;
+  public comments: Array<string> = [];
 
-  constructor(id: string, name: string, desc: string, imagePath: string) {
+  constructor(id: string, name: string, desc: string, imagePath: string, comments: Array<string> = []) {
     this.id = id;
     this.name = name;
     this.description = desc;
     this.imagePath = imagePath;
+    this.comments = comments;
   }
 }
